f8353ee4a2023626f3603e68791aaee34999b612
MishSoft <machiavelli6927@gmail.com>
2024-02-18 00:16:47 +0400

    add colors, styles, add useContext

--- src/components/CardSection/CardImage/CardImage.tsx
+++ src/components/CardSection/CardImage/CardImage.tsx
@@ -3,8 +3,18 @@ import testImage from "/public/Bitcamp/bitcamp-front.svg";
 
 function CardImage() {
   return (
-    <div className="card-image-container">
-      <img className="card-image" src={testImage} />
+    <div
+      className="card-image-container"
+      style={{ backgroundImage: `url(${testImage})` }}
+    >
+      <h2>Card Name</h2>
+      <div className="card-image-details">
+        <h2>0000 0000 0000 0000</h2>
+        <div className="number-date">
+          <h2>mishiko aspanidze</h2>
+          <h2>12/24</h2>
+        </div>
+      </div>
     </div>
   );
 }
